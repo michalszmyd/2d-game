badfa9c408fd7a7323f8fe76963508300c81be57
Michal Szmyd <michal.szmyd@icloud.com>
2018-01-20 15:12:20 +0100

    Add image to item

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180120094315) do
+ActiveRecord::Schema.define(version: 20180120140856) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -89,6 +89,10 @@ ActiveRecord::Schema.define(version: 20180120094315) do
     t.bigint "hero_role_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image_file_name"
+    t.string "image_content_type"
+    t.integer "image_file_size"
+    t.datetime "image_updated_at"
     t.index ["hero_role_id"], name: "index_items_on_hero_role_id"
   end
 
